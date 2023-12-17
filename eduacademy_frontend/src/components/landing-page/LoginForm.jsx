85a87610f742f00bd86918a512d48c78d5b75840
Rawda <52583827+rawdaibrahim@users.noreply.github.com>
2023-12-17 13:41:58 +0200

    Update LoginForm

--- eduacademy_frontend/src/components/landing-page/LoginForm.jsx
+++ eduacademy_frontend/src/components/landing-page/LoginForm.jsx
@@ -4,14 +4,50 @@ import google from '../Assets/google.png'
 
 
 const initialState = {
-    fName: '',
-    lName: '',
     username: '',
+    first_name: '',
+    last_name: '',
+    email:'',
     password: '',
     confirmPassword: '',
     phoneNumber: '',
-    avatarURL: '',
+    governerate: '',
+    phone_number: '',
+    gender: '',
+    user_role:'',
+
 }
+
+
+    const governorates = [
+        'Cairo',
+        'Alexandria',
+        'Ismailia',
+        'Kafr El Sheikh',
+        'Port Said',
+        'Suez',
+        'Dakahlia',
+        'Damietta',
+        'Faiyum',
+        'Gharbia',
+        'Luxor',
+        'Matrouh',
+        'Minya',
+        'Monufia',
+        'New Valley',
+        'North Sinai',
+        'Qalyubia',
+        'Qena',
+        'Red Sea',
+        'Sharqia',
+        'Sohag',
+        'Aswan',
+        'Asyut',
+        'Beni Suef',
+        'Beheira',
+        'Helwan',
+    ];
+
 
 
 
@@ -26,6 +62,7 @@ const Auth = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         window.location.reload();
+
     }
 
     const switchMode = () => {
@@ -65,13 +102,14 @@ const Auth = () => {
                     </div>
                     <form className='inputs' onSubmit={handleSubmit}>
                         {isSignup && (
-
+                            <>
                             <div className="input-box name" id='name'>
                                 <div className="lfname">
                                 <input 
                                     name="fName" 
                                     type="text"
                                     placeholder='First Name'
+                                    value={form.first_name}
                                     onChange={handleChange}
                                     required
                                 />
@@ -81,27 +119,45 @@ const Auth = () => {
                                     name="lName" 
                                     type="text"
                                     placeholder='Last Name'
+                                    value={form.last_name}
                                     onChange={handleChange}
                                     required
                                 />
                                 </div>
                             </div>
+                            <div className="input-box">
+                            <input 
+                                name="email" 
+                                type="email"
+                                placeholder='Email'
+                                value={form.email}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        </>
                         )}
-                        <div className="input-box">
+
+                        {!isSignup && (
+                            <div className="input-box">
                                 <input 
-                                    name="email" 
-                                    type="email"
-                                    placeholder='Email'
+                                    name="username" 
+                                    type="text"
+                                    placeholder="User Name"
+                                    value={form.username}
                                     onChange={handleChange}
                                     required
                                 />
                             </div>
+                        )}
+                        
                         {isSignup && (
                             <div className="input-box">
                                 <input 
                                     name="phoneNumber" 
                                     type="text"
                                     placeholder="Phone Number"
+                                    value={form.phoneNumber}
                                     onChange={handleChange}
                                     required
                                 />
@@ -112,6 +168,7 @@ const Auth = () => {
                                     name="password" 
                                     type="password"
                                     placeholder='Password'
+                                    value={form.password}
                                     onChange={handleChange}
                                     required
                                 />
@@ -122,21 +179,64 @@ const Auth = () => {
                                     name="confirmPassword" 
                                     type="password"
                                     placeholder='confirmPassword'
+                                    value={form.confirmPassword}
                                     onChange={handleChange}
                                     required
                                 />
                             </div>
                             )}
                             {isSignup && (
+                                <div>
+                                <label for="governorateSelect">Select Governorate: </label>
+                                <select
+                                  name='governerate'
+                                  id="governorateSelect"
+                                  value={form.governerate}
+                                  onChange={handleChange}
+                                >
+                                  <option value="">Select...</option>
+                                  {governorates.map((governorate) => (
+                                    <option key={governorate} value={governorate}>
+                                      {governorate}
+                                    </option>
+                                  ))}
+                                </select>
+                          
+                              </div>
+                            )}
+
+                            {isSignup && (
                             <div className="role">
-                                <input type="radio" id='student' value="Student" name="role"/>
+                                Gender:  
+                                <input type="radio" id='Male' value="male" name="gender"
+                                onChange={handleChange }
+                                checked={form.gender === 'male' }
+                                />
+                                <label for="Male" >Male</label>
+                                <input type="radio" id='Female' value="female" name="gender" 
+                                checked={form.gender === 'female'}
+                                onChange={handleChange}/>
+                                <label for="Female" >Female</label>
+                                
+                            </div>
+                            )}
+                            {isSignup && (
+                            <div className="role">
+                                <input type="radio" id='student' value='student' name="user_role"
+                                checked={form.user_role === 'student'}
+                                onChange={handleChange}/>
                                 <label for="student" >Student</label>
-                                <input type="radio" id='teacher' value="Teacher" name="role" />
+                                <input type="radio" id='teacher' value='teacher' name="user_role" 
+                                checked={form.user_role === 'teacher'}
+                                onChange={handleChange}/>
                                 <label for="teacher" >Teacher</label>
-                                <input type="radio" id='assistant' value="assistant" name="role" />
+                                <input type="radio" id='assistant' value='assistant' name="user_role" 
+                                checked={form.user_role === 'assistant'}
+                                onChange={handleChange}/>
                                 <label for="assistant" >Assistant</label>
                             </div>
                             )}
+                            
                         <div className="submit">
                             <button>{isSignup ? "Sign Up" : "Sign In"}</button>
                         </div>
